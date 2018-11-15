873f86c5241147678c1fa173b5453ea36d5bfd83
MarcGallegos <tech.theta@yahoo.com>
2018-11-14 21:53:58 -0700

    stage 2 savepoint item_construct added to display data from db in ListView added empty view layout/strings data displays differently on device

--- app/src/main/java/com/example/android/battledroidscomix_ursearching_4/Adapters/ItemCursorAdapter.java
+++ app/src/main/java/com/example/android/battledroidscomix_ursearching_4/Adapters/ItemCursorAdapter.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import com.example.android.battledroidscomix_ursearching_4.R;
 import com.example.android.battledroidscomix_ursearching_4.data.ComiContract;
+import com.example.android.battledroidscomix_ursearching_4.data.ComiContract.TitleEntry;
 
 public class ItemCursorAdapter extends CursorAdapter {
 
@@ -57,6 +58,8 @@ public class ItemCursorAdapter extends CursorAdapter {
         product_price.setText("Price: " + price);
         product_quantity.setText("In-Stock: " + quantity);
         product_section.setText("Section:" + section);
+
+
     }
 
 }
